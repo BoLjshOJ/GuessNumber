d355258fa3b575ff94a10d29ecf0dd5c3a030cfe
BoLjshOJ <boljshoj@gmial.com>
2018-03-12 14:32:01 +0300

    in GenerateNumberController added method generate(Game game)

--- src/controller/GenerateNumberController.java
+++ src/controller/GenerateNumberController.java
@@ -1,7 +1,17 @@
 package controller;
 
+import model.Game;
+
+import java.util.Random;
+
 public class GenerateNumberController {
 
+    private Random random;
 
+    public int generate(Game game) {
+        final int size = game.getMax() - game.getMin();
+        final int randomValue = random.nextInt() % size;
+        return game.getMin() + randomValue;
+    }
 
 }
